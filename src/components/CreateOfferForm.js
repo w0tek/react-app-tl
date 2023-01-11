06ad98e6a5a1881b3c76622f1b0387c3842ed125
Wojciech Kobylinski <wojciech.kobylinski@ncdc.pl>
2023-01-11 20:19:08 +0100

    Add create offer form.

---
+++ src/components/CreateOfferForm.js
@@ -0,0 +1,81 @@
+import "../index.css";
+import { useState } from "react";
+
+function CreateOfferForm() {
+  const [productName, setProductName] = useState("");
+  const [risk, setRisk] = useState("");
+  const [idNumber, setIdNumber] = useState("");
+  const [price, setPrice] = useState("");
+  const [message, setMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+
+  let handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let res = await fetch("https://httpbin.org/post", {
+        method: "POST",
+        body: JSON.stringify({
+          productName: productName,
+          risk: risk,
+          idNumber: idNumber,
+          price: price,
+        }),
+      });
+      let resJson = await res.json();
+      if (res.status === 200) {
+        setProductName("");
+        setRisk("");
+        setIdNumber("");
+        setPrice("");
+        setMessage("SUCCESS - Offer created");
+        setErrorMessage("");
+      } else {
+        setMessage("");
+        setErrorMessage("ERROR - can't create offer");
+      }
+    } catch (err) {
+        setMessage("");
+      setErrorMessage("ERROR - can't create offer");
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="number"
+          value={idNumber}
+          placeholder="Id number"
+          onChange={(e) => setIdNumber(e.target.value)}
+        />
+        <input
+          type="text"
+          value={productName}
+          placeholder="productName"
+          onChange={(e) => setProductName(e.target.value)}
+        />
+        <input
+          type="text"
+          value={risk}
+          placeholder="risk"
+          onChange={(e) => setRisk(e.target.value)}
+        />
+        <input
+          type="number"
+          value={price}
+          placeholder="Price"
+          onChange={(e) => setPrice(e.target.value)}
+        />
+        <button type="submit">Create offer</button>
+
+        <div className="message">{message ? <p>{message}</p> : null}</div>
+        <div className="errorMessage">{errorMessage ? <p>{errorMessage}</p> : null}</div>
+
+      </form>
+    </div>
+  );
+}
+
+export default CreateOfferForm;
